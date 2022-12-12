181b187409481279f59ff780dff16bb7f7c8d895
hariprasad <harih79562@gmail.com>
2022-12-12 18:37:08 +0530

    removed unused code

--- controller/dataController.js
+++ controller/dataController.js
@@ -5,13 +5,7 @@ const con = require('../config/server');
 
 //get all users data
 exports.getAllData = async(req, res, next)=>{
-    // let result = await resultModel.getAllData();
-    // res.send(
-    //     {   message : "Data Fetched Successfull",
-    //         data : result,
-    //     }
-    // );
-
+  
     let users = con.Collection('users');
     users.find(async(err, data)=>{
         console.log(await data);
