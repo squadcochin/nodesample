b0e2266b821d7a329e05a37ffd3c317a4f336ff4
Nithil-A <nithilpanangattiri@gmail.com>
2022-12-13 09:41:24 +0530

    unused Changes

--- controller/dataController.js
+++ controller/dataController.js
@@ -5,15 +5,16 @@ const con = require('../config/server');
 
 //get all users data
 exports.getAllData = async(req, res, next)=>{
-    // let result = await resultModel.getAllData();
-    // res.send(
-    //     {   message : "Data Fetched Successfull",
-    //         data : result,
-    //     }
-    // );
 
-    let users = con.Collection('users');
-    users.find(async(err, data)=>{
-        console.log(await data);
-    })
+    let result = await resultModel.getAllData();
+    res.send(
+        {   message : "Data Fetched Successfull",
+            data : result,
+        }
+    );
+
+    // let users = con.Collection('users');
+    // users.find(async(err, data)=>{
+    //     console.log(await data);
+    // })
 }
