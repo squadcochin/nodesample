6f263889044a8d693013a16c945a6de42d5b7618
hariprasad <harih79562@gmail.com>
2022-12-13 15:56:15 +0530

    no changes

--- controller/dataController.js
+++ controller/dataController.js
@@ -4,6 +4,8 @@ const con = require('../config/server');
 
 
 //get all users data
+exports.getAllData = async(req, res, next)=>{
+  
     let users = con.Collection('users');
     users.find(async(err, data)=>{
         console.log(await data);
